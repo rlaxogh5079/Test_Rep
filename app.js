17f7db6c0230397035b25e94f651e672ecc7b1a7
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-07-09 03:33:03 +0900

    :tada: test

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const number1 = document.querySelector("#number1")
 const btn1 = document.querySelector("#btn1");
 const error1 = document.querySelector("#error1");
 
-const isPositiveInteger = (number) => {
-    return 0 === number % (!isNaN(parseFloat(number)) && 0 <= ~~number);
+const isPositiveInteger = (target) => {
+    return 0 === target % (!isNaN(parseFloat(target)) && 0 <= ~~target);
 }
 
 const checkNumber = (target) => {
@@ -17,18 +17,35 @@ const runError = (className) => {
     }, 1000);
 }
 
-var value = "";
-
-number1.addEventListener("keypress", (event) => {
-    event.preventDefault();
-    if (checkNumber(event.target.value) || event.target.value == "") {
-        if (event.target.value < 999){
+number1.addEventListener("keydown", (event) => {
+    var value = event.target.value;
+    event.preventDefault()
+    if (checkNumber(event.key) || event.target.value == "") {
+        if (event.key === "Backspace") {
+            console.log(event.target.value)
+            if (Number(event.target.value) == 0 || event.target.value == ""){
+                alert("숫자를 먼저 입력하세요.")
+            }
+            value = Number.parseInt(Number(value) / 10).toString().padStart(4, "0");
+            event.target.value = value;
+        } else if (!checkNumber(event.key)) {
+            runError(number1)
+            runError(error1)
+            error1.innerText = "숫자만 입력하세요.";
+        } else if (event.target.value < 1000){
             error1.InnerText = "";
-            value += event.key
+            value = Number(value) +  event.key
             event.target.value = value.padStart(4, "0");
         } else {
             alert("4글자 이하로 입력하세요!")
         }
+    } else if (event.key === "Backspace") {
+        console.log(event.target.value)
+        if (Number(event.target.value) == 0 || event.target.value == ""){
+            alert("숫자를 먼저 입력하세요.")
+        }
+        value = Number.parseInt(Number(value) / 10).toString().padStart(4, "0");
+        event.target.value = value;
     } else {
         runError(number1)
         runError(error1)
@@ -40,4 +57,5 @@ btn1.addEventListener("click", (event) => {
     console.log(number1.value);
     event.preventDefault();
     number1.value = "";
+    value = "";
 })
